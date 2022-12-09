912b3aa181e4f8a7e644b023a4f25450ff45d932
Sean van Dijk <sean.vandijk@kpn.com>
2022-12-09 15:49:17 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,43 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var current = 0
+        var max = 0
+        for (line in input) {
+            if (line.isBlank()) {
+                if (current > max) {
+                    max = current
+                }
+                current = 0
+            } else {
+                current += line.toInt()
+            }
+        }
+        return max
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun retrieveTotalCaloriesPerElf(input: String): Sequence<Int> {
+        return input
+            // Group the snacks held by a single elf to their own string
+            .splitToSequence("\n\n")
+            .map { singleElfSnacksString ->
+                // Split to a string per snack
+                singleElfSnacksString.splitToSequence("\n")
+                    .filter { it.isNotBlank() }
+                    // convert them to integers
+                    .map { it.toInt() }
+                    // And sum them to get the total calories per elf
+                    .sum()
+            }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part1v2(input: String) = retrieveTotalCaloriesPerElf(input).max()
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    fun part2(input: String) = retrieveTotalCaloriesPerElf(input)
+        .sortedDescending()
+        .take(3)
+        .sum()
+
+    println(part1(readInput("Day01")))
+    println(part1v2(readInputAsString("Day01")))
+    println(part2(readInputAsString("Day01")))
 }
